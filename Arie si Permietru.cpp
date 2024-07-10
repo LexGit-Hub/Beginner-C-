b301718e29013f4a893dfc1701c6185b75b5216a
Lex <101748165+LexGit-Hub@users.noreply.github.com>
2024-07-10 15:16:00 +0300

    Update Arie si Permietru.cpp

--- Arie si Permietru.cpp
+++ Arie si Permietru.cpp
@@ -2,17 +2,18 @@
 /*Fie r un numar real citit la tastatura, care reprezinta lungimea razei unui cerc. Sa se srie un program care sa calculeze aria si perimetrul discului de raza r.*/
 
 #include <iostream>
+#include <cmath>
 using namespace std;
-float r, A, pi=3.14, P;
+float r, A, P;
 int main(){
 system("title A si P cerc");
     
     cout <<"r= "; cin >> r;
-    A = pi*r*r;
-    P = 2*pi*r;
+    A = M_PI*r*r;
+    P = 2*M_PI*r;
 
 cout << "Aria cercului este " << A << " cm"<<  endl;
-cout << "Perimetrul cercului este"<< P <<" cm"<< endl;
+cout << "Perimetrul cercului este "<< P <<" cm"<< endl;
     
 system("pause");
 return 0;
